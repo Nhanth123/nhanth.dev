f6a48cd5758a39109c11df59c8038e2d533af9fd
nhan.trinh <hongnhan50@gmail.com>
2023-11-29 06:37:05 +0700

    Add project astro into nhanth.dev

--- scully.nhanth-dev.config.ts
+++ scully.nhanth-dev.config.ts
@@ -6,5 +6,12 @@ export const config: ScullyConfig = {
   distFolder: 'site/nhanth-dev', // output directory of your Angular build 
   outDir: './site/static', // directory for scully build artifacts
   defaultPostRenderers: [],
-  routes: {},
+  routes: {
+    '/blog/:slug':{
+      type: 'blog',
+      slug: {
+        folder: "./blog"
+      }
+    }
+  },
 };
